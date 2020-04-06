a2a7bc52120716a2d4938404ee7fbb1c0cd644ae
Lucas Carrias <lucascarrias@outlook.com>
2020-04-06 16:59:47 -0300

    Adcionar e remover itens da lista.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,19 @@
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass('completed');
 });
 
-$(".deleteIcon").on("click", function (event) {
+$("ul").on("click", ".deleteIcon", function (event) {
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function (event) {
+    if (event.which === 13){
+        textContent = $(this).val();
+        $(this).val("");
+
+        $("ul").append("<li> <span class=\"deleteIcon\">X</span> "+ textContent +"</li>");
+    }
 });
